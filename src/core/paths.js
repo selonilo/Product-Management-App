6fe739c9b26c289d65b40c23d1f6fba3974b688a
kerimkeskin <g170900033@subu.edu.tr>
2021-10-23 16:04:02 +0300

    Refactor login register paths

--- src/core/paths.js
+++ src/core/paths.js
@@ -2,5 +2,5 @@
 
 export const baseUrl = 'http://localhost:3001/';
 
-export const login = baseUrl + 'api/auth/signup';
-export const register = baseUrl + 'api/auth/signin';
+export const login = baseUrl + 'api/auth/signin';
+export const register = baseUrl + 'api/auth/signup';
