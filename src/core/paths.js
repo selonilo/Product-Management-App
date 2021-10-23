92aad47e11d89c0c24f84653866c0c1f136faec1
kerimkeskin <g170900033@subu.edu.tr>
2021-10-23 16:23:49 +0300

    Refactor add product service

--- src/core/paths.js
+++ src/core/paths.js
@@ -4,3 +4,4 @@ export const baseUrl = 'http://localhost:3001/';
 
 export const login = baseUrl + 'api/auth/signin';
 export const register = baseUrl + 'api/auth/signup';
+export const addProduct = baseUrl + 'api/product/create';
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -9,16 +9,10 @@ import Item from './Item';
 
 const HomeScreen = ({navigation}) => {
   const [product, setProduct] = useState({
-    brand: {
-      name: 'Apple',
-    },
-    category: {
-      name: 'aksesuar',
-    },
+    brand: 'Apple',
+    category: 'Phone',
     name: 'Apple Watch',
-    stock: {
-      amount: 5,
-    },
+    quantity: 2,
   });
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState([]);
@@ -38,11 +32,15 @@ const HomeScreen = ({navigation}) => {
     services
       .addProduct(product)
       .then(res => {
-        setResult([...result, res]);
+        alert(res?.message);
       })
       .catch(err => console.log(err))
       .finally(res => setLoading(false));
   };
+
+  const getAll = () => {
+    
+  }
   return (
     <View style={styles.container}>
       <Button
@@ -54,14 +52,14 @@ const HomeScreen = ({navigation}) => {
         }}>
         <Text>ÜRÜN EKLE</Text>
       </Button>
-      {/* <Button mode="contained" style={styles.button} onPress={() => logout()}>
+      <Button mode="contained" style={styles.button} onPress={() => logout()}>
         <Text>ÇIKIŞ YAP</Text>
-      </Button> */}
-      <FlatList
+      </Button>
+      {/* <FlatList
         data={result}
         keyExtractor={(item, index) => item?.id}
         renderItem={({item}) => <Item item={item} />}
-      />
+      /> */}
     </View>
   );
 };
