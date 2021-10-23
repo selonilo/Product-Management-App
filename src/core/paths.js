27f87402561daac55cc041700fbb6bfd03d34986
kerimkeskin <g170900033@subu.edu.tr>
2021-10-23 22:25:10 +0300

    Add refreshToken,addProduct,getAllProduct service configs

--- src/core/paths.js
+++ src/core/paths.js
@@ -4,4 +4,7 @@ export const baseUrl = 'http://localhost:3001/';
 
 export const login = baseUrl + 'api/auth/signin';
 export const register = baseUrl + 'api/auth/signup';
-export const addProduct = baseUrl + 'api/product/create';
+export const refreshToken = baseUrl + 'api/auth/refreshtoken';
+
+export const addProduct = baseUrl + 'api/create/product';
+export const getAllProduct = baseUrl + 'api/getAllProduct';
